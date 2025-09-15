854a08703463cd8de49a8cd535451ef5a16d9b16
ApiuKur <akur@macalester.edu>
2025-09-14 22:58:31 -0500

    created and edited the buggyscripts
    created and edited all the files

---
+++ src/ica/ApiuKurfirstturtleprogram.py
@@ -0,0 +1,20 @@
+import turtle
+#wn=turtle.Screen()
+#alex=turtle.Turtle()
+#alex.forward(150)
+#alex.left(90)
+#alex.forward(75)
+import turtle
+
+import turtle
+wn=turtle.Screen()
+wn.bgcolor('lightgreen')
+wn.exitonclick()
+tess=turtle.Turtle()
+tess.color('hotpink')
+tess.pensize(5)
+alex=turtle.Turtle()
+tess.forward(80)
+tess.left(
+    
+)
---
+++ src/ica/turtle/triangle.py
@@ -0,0 +1,10 @@
+import turtle
+
+window=turtle.Screen()
+
+my_turtle=turtle.Turtle()
+my_turtle.color("blue")
+my_turtle.forward(50)
+my_turtle.left(45)
+my_turtle.forward(50)
+window.exitonclick()
